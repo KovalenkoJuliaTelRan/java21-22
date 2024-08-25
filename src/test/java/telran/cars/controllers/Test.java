0fda12af254e8bd21e6b8f8f5cb483cf0164a1f4
Kovalenko Julia <kovalenko.julietta@mail.ru>
2024-08-25 19:54:15 +0300

    add test file

---
+++ src/test/java/telran/cars/controllers/Test.java
@@ -0,0 +1,27 @@
+package telran.cars.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class Test
+{
+
+	@BeforeEach
+	void setUp() throws Exception
+	{
+	}
+
+	@AfterEach
+	void tearDown() throws Exception
+	{
+	}
+
+	@org.junit.jupiter.api.Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
